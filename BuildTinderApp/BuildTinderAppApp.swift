a73d53c892a7dd37891213e74f9c93d94716bbf9
Riley Lee <riley.lee@gocity.com>
2022-12-03 21:07:33 +0000

    Initial Commit

---
+++ BuildTinderApp/BuildTinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTinderAppApp.swift
+//  BuildTinderApp
+//
+//  Created by Riley Lee on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BuildTinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
