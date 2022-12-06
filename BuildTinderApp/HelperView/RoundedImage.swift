173c98b872f5c5d75a6d432c1f41e4338b6cc940
Riley Lee <riley.lee@gocity.com>
2022-12-06 19:32:05 +0000

    finished 3.5

---
+++ BuildTinderApp/HelperView/RoundedImage.swift
@@ -0,0 +1,26 @@
+//
+//  RoundedImage.swift
+//  BuildTinderApp
+//
+//  Created by Riley Lee on 06/12/2022.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct RoundedImage: View {
+    var url: URL?
+    
+    var body: some View {
+        KFImage(url)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+    }
+}
+
+struct RoundedImage_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedImage(url: URL(string: "https://picsum.photos/400"))
+    }
+}
